a0a5171d850e2c2349f18755c7d7ffde440417f2
Adam Lesnikowski <adam.lesnikowski@gmail.com>
2016-07-18 19:16:46 -0400

    Update renameDeepFeats.sh

--- renameDeepFeats.sh
+++ renameDeepFeats.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #Replaces all files like Pics123456.npy with 123456.npy
 #Perl regex has format 's/OLD/NEW/'
+#replace -n (dry run flag) with -v to actually carry out replacment, with verbose flag
 rename -v 's/Pics(\d*)/$1/' *.npy
-#replace -n with -v to actually carry out replacment, with verbose flag
+
