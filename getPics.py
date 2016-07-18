cedacea46fbaa3c12869338251494785a2b3edc8
Adam Lesnikowski <adam.lesnikowski@gmail.com>
2016-07-18 19:09:09 -0400

    Create getPics.py

---
+++ getPics.py
@@ -0,0 +1,41 @@
+#Get house share photos script
+#Optionally run interactively in iPython, Jupyter and examine the db data frame or call getData(0, howeverManyImages)
+
+import sys
+import os
+import pandas as pd
+import numpy
+import urllib
+import time
+
+#City to read listings file from. In this example Paris.
+#Set read_csv_loc as location of where you downloaded and unpacked /data/listings.csv.gz from pullCSVs.py
+read_csv_loc = '/home/adam/CVProjects/AirBnBConvNet/Data/PAR/listingsBig.csv'
+#Set out_image_base to wherever you want the image data to go
+out_image_base = '/home/adam/CVProjects/AirBnBConvNet/Data/Pics/PAR/'
+
+#pandas work
+db = pd.read_csv(read_csv_loc)
+print(db.keys())
+urls = db['xl_picture_url']
+dict = urls.to_dict()
+values = dict.values()
+length = len(values)
+
+def getData(start, end):
+    for i in xrange(start, end):
+        delay =  abs(numpy.random.randn()) #samples from normal with mean 0, variance 1
+        time.sleep(delay)  #Be nice to the servers
+        print("Delayed for %s seconds" % delay)
+        try:
+            link = values[i]
+            id = db.id[i]
+            out_image_location = out_image_base + str(id) + '.jpg'
+            urllib.urlretrieve(link, out_image_location)
+            print("Got image %s" % id)
+        except:
+            e = sys.exc_info()[0]  #Pulls error info
+            print("<p>Error: %s </p>" % e )
+
+#Get all the data!
+getData(0, length)
