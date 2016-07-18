abe4e783508158fe5b8758640e638468da3f334a
Adam Lesnikowski <adam.lesnikowski@gmail.com>
2016-07-18 19:15:12 -0400

    Create renamePics.sh

---
+++ renamePics.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#renamePics.sh
+#Replaces all files like Pics123456.jpg with 123456.jpg
+#Perl regex has format 's/OLD/NEW/'
+rename -v 's/Pics(\d*)/$1/' *.jpg
+#replace -n (dry run) flag with -v to actually carry out replacment, with verbose flag
